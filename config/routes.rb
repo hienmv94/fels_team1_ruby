e350a1b79603cd53367064a9c6e95fcf83fe22b5
Hienmv94 <hienmv94@gmail.com>
2015-11-12 13:47:46 +0700

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   get "about" => 'static_pages#about'
   get "contact" => 'static_pages#contact'
   
-  namespace "admin" do
+  namespace :admin do
     root 'static_pages#home'
     resources :users
+    resources :categories
   end
 
 end
